6078237931e6bace1ed46eabd64e93a8827783e4
pach092 <pachobernal5@gmail.com>
2017-12-09 18:03:02 -0500

    Estructuras selectivas en C

---
+++ estructuraSelectiva/ejemploSelectiva.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void main(){
+/*
+    int a = 10;
+    int b = 0;
+
+    if(a > b){
+        printf("Efectivamente, a es mayor que b\n");
+        printf("Saludos.");
+    }
+
+    printf("El programa se va a cerrar.\n");
+*/
+    int a = 1;
+    int b = 0;
+
+    if(a == 0){
+        printf("a es un numero igual a 0");
+    } else {
+        printf("a puede ser cualquier otro numero diferente de 0");
+    }
+}
