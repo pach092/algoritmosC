6b5ae80391348b39edcfc734e97b0263733dab05
pach092 <pachobernal5@gmail.com>
2017-12-09 18:10:05 -0500

    Estructuras selectivas con else if en C

--- estructuraSelectiva/ejemploSelectiva.c
+++ estructuraSelectiva/ejemploSelectiva.c
@@ -12,6 +12,7 @@ void main(){
 
     printf("El programa se va a cerrar.\n");
 */
+/*  
     int a = 1;
     int b = 0;
 
@@ -19,5 +20,23 @@ void main(){
         printf("a es un numero igual a 0");
     } else {
         printf("a puede ser cualquier otro numero diferente de 0");
+    }
+*/
+
+    int a, b, c, d;
+
+    scanf("%d", &a);
+    scanf("%d", &b);
+    scanf("%d", &c);
+    scanf("%d", &d);
+
+    if (a > b){
+        printf("a es mayor que b\n");
+    } else if (a > c){
+        printf("a es mayor que c\n");
+    } else if (a > d){
+        printf("a es mayor que d\n");
+    } else{
+        printf("a no es mayor que b, c o d\n");
     }
 }
