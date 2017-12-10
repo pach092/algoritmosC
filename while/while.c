fba4ea0891f3afbadf13eaa88285f9cf5b3c3f16
pach092 <pachobernal5@gmail.com>
2017-12-09 19:48:15 -0500

    Estructura Do While en C

--- while/while.c
+++ while/while.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 void main(){
+    /*
     int decision = 1;
     int incrementador = 0;
 
@@ -10,5 +11,16 @@ void main(){
         printf("Si desea continuar presione 1 o cualquier otra para salir: ");
         scanf("%d", &decision);
     }
+    printf("Gracias, adios");
+    */
+    int decision;
+    int incrementador = 0;
+
+    do{
+        incrementador++;
+        printf("Ejecucion #%d\n", incrementador);
+        printf("Si desea continuar presione 1 o cualquier otra para salir: ");
+        scanf("%d", &decision);
+    }while(decision == 1);
     printf("Gracias, adios");
 }
