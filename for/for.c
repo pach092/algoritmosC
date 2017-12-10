05209ccf2d7448f3bad48b7a30347d58475f22d8
pach092 <pachobernal5@gmail.com>
2017-12-09 19:55:59 -0500

    Estructura For en C

---
+++ for/for.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main(){
+    int variable;
+
+    for(variable = 1; variable <= 10; variable++){
+        printf("Tu variable vale: %d\n", variable);
+    }
+    printf("Gracias, adios");
+}
