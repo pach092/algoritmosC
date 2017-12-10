48e1d07ab05b99e4e99ceb3ab87ba02b8b0846e6
pach092 <pachobernal5@gmail.com>
2017-12-09 20:40:29 -0500

    Finalizando tienda

--- platziStore/platziStore.c
+++ platziStore/platziStore.c
@@ -6,6 +6,12 @@
 char opcion[8];
 int gorras;
 int totalGorras = 0;
+int termo;
+int totalTermo = 0;
+int playera;
+int totalPlayera = 0;
+int taza;
+int totalTaza = 0;
 int seguirComprando = 1;
 
 void main(){
@@ -23,11 +29,20 @@ void main(){
             totalGorras = totalGorras + (gorras * 5);
             printf("Su total de gorras es: %d\n", totalGorras);
         }else if(strcmp(opcion, "termo") == 0){
-            
+            printf("Cada termo cuesta $10, cuantos quieres?: ");
+            scanf("%d", &termo);
+            totalTermo = totalTermo + (termo * 10);
+            printf("Su total de gorras es: %d\n", totalTermo);
         }else if(strcmp(opcion, "playera") == 0){
-            
+            printf("Cada playera cuesta $15, cuantas quieres?: ");
+            scanf("%d", &playera);
+            totalPlayera = totalPlayera + (playera * 15);
+            printf("Su total de gorras es: %d\n", totalPlayera);
         }else if(strcmp(opcion, "taza") == 0){
-            
+            printf("Cada taza cuesta $20, cuantas quieres?: ");
+            scanf("%d", &taza);
+            totalTaza = totalTaza + (taza * 15);
+            printf("Su total de gorras es: %d\n", totalTaza);
         }else{
             printf("El producto especificado no existe, prueba otra opcion\n");
             printf("En caso de querer otro producto presion 1, para salir presione cualquier numero\n");
